cbfa9b9b7158aa928bc80facba08c2843fc0aebb
Kimura Yuki <s21c1039xx@s.chibakoudai.jp>
2022-10-27 12:45:13 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 #SPDX-fileCopyrightText: 2022 Kimura Yuki
 #SPDX-license-Identifier: BSD-3-Clause
 
@@ -13,6 +13,14 @@ res=0
 
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
+###STRANGE INPUT###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]		|| ng ${LINENO}
+[ "${out}" = "" ]	|| ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]		|| ng ${LINENO}
+[ "${out}" = "" ]	|| ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 
